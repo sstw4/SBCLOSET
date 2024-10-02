4d28c50dd53934f17cb0a5883b48e87ea9833caf
sstw4 <daniellelive05@gmail.com>
2024-10-02 12:05:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper-1",{
+    slidesPerView:1,
+    spaceBetween:30,
+    loop:true,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    navigation:{
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-buttom-prev",
+    }
